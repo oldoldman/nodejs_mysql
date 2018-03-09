a7bdda406cc144df3e2c24db98162bd02883e10e
oldoldman <yushang@outlook.com>
2018-03-10 06:52:43 +0800

    initial commit

--- mysql.js
+++ mysql.js
@@ -1,25 +1,23 @@
 const _my = require('./_mysql')
 
 function *rset(cn,query) {
-	var res = _my.query(cn.handle,query,true)
-	while((r = _my.fetch_row(res)) != null)
-		yield r;
-}
-
-function query(cn,query) {
-	return _my.query(cn.handle,query,false);
+  var res = _my.query(cn.handle,query,true);
+  if (res == null ) return;
+  while((r = _my.fetch_row(res)) != null)
+    yield r;
 }
 
 function test() {
-	var g = rset(_my.connect(),'select * from user');
-	var r = g.next();
-	do {
-		console.log(r.value);
-		r = g.next();	
-	}while(!r.done)
+  var g = rset(_my.connect(),'select * from user');
+  var r = g.next();
+  do {
+    console.log(r.value);
+    r = g.next();	
+  }while(!r.done)
 }
 
 exports.connect = ()=>{return _my.connect();};
+exports.query = (cn,query)=>{return _my.query(cn.handle,query,false);};
 exports.rset = rset;
 exports.test = test;
-exports.query = query;
+

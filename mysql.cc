6e0303576641ce4dcc9f1685f8a702a1cf1bfd81
oldoldman <yushang@outlook.com>
2018-03-10 08:56:15 +0800

    initial commit

--- mysql.cc
+++ mysql.cc
@@ -16,6 +16,42 @@ using v8::PropertyAttribute;
 using v8::String;
 using v8::Null;
 using v8::Value;
+using v8::Uint32;
+using v8::PropertyCallbackInfo;
+
+#define MYSQL_SERVER_STATUS 1
+#define MYSQL_CLIENT_FLAG 2
+#define MYSQL_STATUS 3
+#define MYSQL_WARNING_COUNT 4
+#define MYSQL_AFFECTED_ROWS 5
+
+void get_MYSQL_field(Local<String> property,
+  const PropertyCallbackInfo<Value>& info) {
+  Isolate *isolate = info.GetIsolate();
+  Local<Value> handle = info.This()->Get(String::NewFromUtf8(isolate,"handle"));
+  Local<External> external = Local<External>::Cast(handle);
+  Uint32 *idx = Uint32::Cast(*(info.Data()));
+  MYSQL *cn = (MYSQL*)external->Value();
+  
+  switch (idx->Value())
+  {
+    case MYSQL_SERVER_STATUS:
+      info.GetReturnValue().Set((int32_t)cn->server_status);
+      break;
+    case MYSQL_CLIENT_FLAG:
+      info.GetReturnValue().Set((int32_t)cn->client_flag);
+      break;
+    case MYSQL_STATUS:
+      info.GetReturnValue().Set((int32_t)cn->status);
+      break;
+    case MYSQL_WARNING_COUNT:
+      info.GetReturnValue().Set((int32_t)cn->warning_count);
+      break;
+    // case MYSQL_AFFECTED_ROWS:
+    default:
+      info.GetReturnValue().Set((int32_t)cn->affected_rows);
+  }
+}
 
 void my_connect(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
@@ -32,6 +68,11 @@ void my_connect(const FunctionCallbackInfo<Value>& args) {
   Local<String> host_info = String::NewFromUtf8(isolate,"host_info");
   Local<String> info = String::NewFromUtf8(isolate,"info");
   Local<String> db = String::NewFromUtf8(isolate,"db");
+  Local<String> client_flag = String::NewFromUtf8(isolate,"client_flag");
+  Local<String> server_status = String::NewFromUtf8(isolate,"server_status");
+  Local<String> status = String::NewFromUtf8(isolate,"status");
+  Local<String> warning_count = String::NewFromUtf8(isolate,"warning_count");
+  Local<String> affected_rows = String::NewFromUtf8(isolate,"affected_rows");
 
   PropertyAttribute dont_delete_enum = static_cast<PropertyAttribute>(v8::DontDelete|v8::DontEnum);
   PropertyAttribute dont_delete = static_cast<PropertyAttribute>(v8::DontDelete);
@@ -45,6 +86,11 @@ void my_connect(const FunctionCallbackInfo<Value>& args) {
   objt->Set(host_info,ObjectTemplate::New(isolate),dont_delete);
   objt->Set(info,ObjectTemplate::New(isolate),dont_delete);
   objt->Set(db,ObjectTemplate::New(isolate),dont_delete);
+  objt->SetAccessor(client_flag,get_MYSQL_field,0,Uint32::New(isolate,MYSQL_CLIENT_FLAG));
+  objt->SetAccessor(server_status,get_MYSQL_field,0,Uint32::New(isolate,MYSQL_SERVER_STATUS));
+  objt->SetAccessor(status,get_MYSQL_field,0,Uint32::New(isolate,MYSQL_STATUS));
+  objt->SetAccessor(warning_count,get_MYSQL_field,0,Uint32::New(isolate,MYSQL_WARNING_COUNT));
+  objt->SetAccessor(affected_rows,get_MYSQL_field,0,Uint32::New(isolate,MYSQL_AFFECTED_ROWS));  
 
   Local<Object> obj = objt->NewInstance();
   obj->Set(handle,External::New(isolate,con));
--- mysql.js
+++ mysql.js
@@ -1,5 +1,52 @@
 const _my = require('./_mysql')
 
+const SERVER_STATUS_IN_TRANS = 1;
+const SERVER_STATUS_AUTOCOMMIT = 2;
+const SERVER_MORE_RESULTS_EXISTS = 8;
+const SERVER_QUERY_NO_GOOD_INDEX_USED = 16;
+const SERVER_QUERY_NO_INDEX_USED = 32;
+const SERVER_STATUS_CURSOR_EXISTS = 64;
+const SERVER_STATUS_LAST_ROW_SENT = 128;
+const SERVER_STATUS_DB_DROPPED = 256;
+const SERVER_STATUS_NO_BACKSLASH_ESCAPES = 512;
+const SERVER_STATUS_METADATA_CHANGED = 1024;
+const SERVER_QUERY_WAS_SLOW = 2048;
+const SERVER_PS_OUT_PARAMS = 4096;
+const SERVER_STATUS_IN_TRANS_READONLY = 8192;
+const SERVER_SESSION_STATE_CHANGED = (1<<14);
+
+const CLIENT_LONG_PASSWORD = 1;
+const CLIENT_FOUND_ROWS = 2;
+const CLIENT_LONG_FLAG = 4;
+const CLIENT_CONNECT_WITH_DB = 8;
+const CLIENT_NO_SCHEMA = 16;
+const CLIENT_COMPRESS = 32;
+const CLIENT_ODBC = 64;
+const CLIENT_LOCAL_FILES = 128;
+const CLIENT_IGNORE_SPACE = 256;
+const CLIENT_PROTOCOL_41 = 512;
+const CLIENT_SSL = 1024;
+const CLIENT_IGNORE_SIGPIPE = 4096;
+const CLIENT_TRANSACTIONS = 8192;
+const CLIENT_RESERVED = 16384;
+const CLIENT_RESERVED2 = 32768;
+const CLIENT_MULTI_STATEMENTS = (1<<16);
+const CLIENT_MULTI_RESULTS = (1<<17);
+const CLIENT_PS_MULTI_RESULTS = (1<<18);
+const CLIENT_PLUGIN_AUTH = (1<<19);
+const CLIENT_CONNECT_ATTRS = (1<<20);
+const CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = (1<<21);
+const CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = (1<<22);
+const CLIENT_SESSION_TRACK = (1<<23);
+const CLIENT_DEPRECATE_EOF = (1<<24);
+const CLIENT_SSL_VERIFY_SERVER_CERT = (1<<30);
+const CLIENT_REMEMBER_OPTIONS = (1<<31);
+
+function connect() {
+  var cn = _my.connect();
+  return cn;
+}
+
 function *rset(cn,query) {
   var res = _my.query(cn.handle,query,true);
   if (res == null ) return;
@@ -16,7 +63,7 @@ function test() {
   }while(!r.done)
 }
 
-exports.connect = ()=>{return _my.connect();};
+exports.connect = connect;
 exports.query = (cn,query)=>{return _my.query(cn.handle,query,false);};
 exports.rset = rset;
 exports.test = test;
